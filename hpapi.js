31e590ab442a59ffc090c2705ae52695bdd9e8f8
mark-page-uk <mark.page@whitelamp.com>
2018-10-24 19:30:46 +0100

    Corrected white space to left-indent var keyword

--- hpapi.js
+++ hpapi.js
@@ -1,10 +1,10 @@
 
 (function ( ) {
 
-    var hpapi = {
+var hpapi = {
 
         _init : function ( ) {
-            var frm     = document.getElementById ('hpapi-new');
+        var frm                             = document.getElementById ('hpapi-new');
             frm.reset ();
             try {
                 frm.new.addEventListener('click',this.create.bind(this));
@@ -18,33 +18,33 @@
             buttonEvent.preventDefault ();
             this.log ('hpapi:create(): creating new client request');
             try {
-                var frm                 = buttonEvent.target.form;
+             var frm                        = buttonEvent.target.form;
                 if (frm.txnid.value.length==0) {
                     this.log ('hpapi.create(): txnid was not given');
                     return;
                 }
-                var r = new RegExp (/^https?\:\/\/[^\s]+/);
+            var r                           = new RegExp (/^https?\:\/\/[^\s]+/);
                 if (!r.test(frm.url.value)) {
                     this.log ('hpapi.create(): URL is not valid');
                     return;
                 }
-                var tid                 = frm.txnid.value;
-                var jps                 = frm.json_pretty_string.value;
-                var obj                 = {
-                        txnid : tid
-                       ,key : frm.key.value
-                       ,email : frm.email.value
-                       ,password : frm.password.value
-                       ,datetime : new Date().toISOString()
-                       ,method : {
-                            vendor : frm.vendor.value
-                           ,package : frm.package.value
-                           ,class : frm.class.value
-                           ,method : frm.method.value
-                           ,arguments : new Array ()
-                        }
+            var tid                         = frm.txnid.value;
+            var jps                         = frm.json_pretty_string.value;
+            var obj                         = {
+                    txnid : tid
+                   ,key : frm.key.value
+                   ,email : frm.email.value
+                   ,password : frm.password.value
+                   ,datetime : new Date().toISOString()
+                   ,method : {
+                        vendor : frm.vendor.value
+                       ,package : frm.package.value
+                       ,class : frm.class.value
+                       ,method : frm.method.value
+                       ,arguments : new Array ()
                     }
-                var args                = frm.getElementsByClassName ('argument');
+                }
+            var args                        = frm.getElementsByClassName ('argument');
                 for (i in args) {
                     if (parseInt(i)!=i) {
                         continue;
@@ -53,20 +53,20 @@
                         obj.method.arguments.push (args[i].value);
                     }
                     else {
-                        args[i].value   = '';
+                        args[i].value       = '';
                     }
                 }
-                var pst                 = document.getElementById ('hpapi-template').cloneNode(true);
+            var pst                         = document.getElementById ('hpapi-template').cloneNode(true);
                 pst.setAttribute ('id',tid);
-                pst.txnid.value         = frm.txnid.value;
-                pst.url.value           = frm.url.value;
-                pst.timeout.value       = frm.timeout.value;
-                pst.key.value           = frm.key.value;
-                pst.email.value         = frm.email.value;
-                pst.password.value      = frm.password.value;
-                pst.json.value          = JSON.stringify (obj,null,jps);
+                pst.txnid.value             = frm.txnid.value;
+                pst.url.value               = frm.url.value;
+                pst.timeout.value           = frm.timeout.value;
+                pst.key.value               = frm.key.value;
+                pst.email.value             = frm.email.value;
+                pst.password.value          = frm.password.value;
+                pst.json.value              = JSON.stringify (obj,null,jps);
                 document.body.appendChild (pst);
-                frm.password.value      = '';
+                frm.password.value          = '';
                 pst.post.addEventListener ('click',this.post.bind(this));
                 this.log ('hpapi:create(): listening for post button');
                 this.log ('hpapi:create(): reporting request is created');
@@ -81,8 +81,8 @@
 
        ,log : function (str) {
             try {
-                this.logger         = document.getElementById('hpapi-logger').getElementsByTagName('textarea')[0];
-                this.logger.value  += str + '\n';
+                this.logger                 = document.getElementById('hpapi-logger').getElementsByTagName('textarea')[0];
+                this.logger.value          += str + '\n';
             }
             catch (e) {
                 console.log (str);
@@ -94,134 +94,134 @@
             buttonEvent.preventDefault ();
             this.log ('hpapi.post(): attempting request');
             try {
-                var frm = buttonEvent.target.form;
-                var tgt = frm.json;
+                var frm                     = buttonEvent.target.form;
+                var tgt                     = frm.json;
             }
             catch (e) {
                 this.log ('hpapi.post(): '+e);
                 return;
             }
-            var lfn = this.log;
-            var tid = frm.txnid.value;
-            var url = frm.url.value;
-            var toc = 1000*frm.timeout.value;
-            var rdy = frm.ready;
-            var sts = frm.status;
-            var cod = frm.code;
-            var erm = frm.error;
-            var wnm = frm.warning;
-            var ntc = frm.notice;
-            var xhr = new XMLHttpRequest ();
+        var lfn                             = this.log;
+        var tid                             = frm.txnid.value;
+        var url                             = frm.url.value;
+        var toc                             = 1000*frm.timeout.value;
+        var rdy                             = frm.ready;
+        var sts                             = frm.status;
+        var cod                             = frm.code;
+        var erm                             = frm.error;
+        var wnm                             = frm.warning;
+        var ntc                             = frm.notice;
+        var xhr = new XMLHttpRequest ();
             // Event handlers define
-                xhr.onreadystatechange = function() {
-                    for (var idx in sts.options) {
-                        if (sts.options[idx].value==xhr.readyState) {
-                            sts.selectedIndex = idx;
-                            break;
-                        }
+            xhr.onreadystatechange          = function() {
+                for (var idx in sts.options) {
+                    if (sts.options[idx].value==xhr.readyState) {
+                        sts.selectedIndex   = idx;
+                        break;
                     }
-                    if(xhr.readyState!=4) {
-                        return;
-                    }
-                    if (xhr.responseText.length==0) {
-                        lfn ('txnid '+tid+', onreadystatechange(): READY STATE 4, NO RESPONSE TEXT');
-                        return;
-                    }
-                    for (var idx in sts.options) {
-                        if (sts.options[idx].value==xhr.status) {
-                            sts.selectedIndex = idx;
-                            break;
-                        }
-                    }
-                    lfn ('txnid '+tid+', onreadystatechange(): READY STATE 4, RESPONSE TEXT FOUND');
-                    tgt.value = xhr.responseText; 
                 }
-                xhr.onerror = function() {
-                    for (var idx in sts.options) {
-                        if (sts.options[idx].value==xhr.status) {
-                            sts.selectedIndex = idx;
-                            break;
-                        }
-                    }
-                    tgt.value = '002 HTTP error '+xhr.status;
-                    lfn ('txnid '+tid+', onerror(): HTTP ERROR');
+                if(xhr.readyState!=4) {
+                    return;
                 }
-                xhr.ontimeout = function() {
-                    for (var idx in sts.options) {
-                        if (sts.options[idx].value==599) {
-                            sts.selectedIndex = idx;
-                            break;
-                        }
-                    }
-                    tgt.value = '001 Request timed out';
-                    lfn ('txnid '+tid+', ontimeout(): CONNECTION TIMED OUT');
+                if (xhr.responseText.length==0) {
+                    lfn ('txnid '+tid+', onreadystatechange(): READY STATE 4, NO RESPONSE TEXT');
+                    return;
                 }
-                xhr.onloadend = function() {
-                    var s = xhr.status;
-                    if (s==0) {
-                        s = 999;
+                for (var idx in sts.options) {
+                    if (sts.options[idx].value==xhr.status) {
+                        sts.selectedIndex   = idx;
+                        break;
                     }
-                    for (var idx in sts.options) {
-                        if (sts.options[idx].value==s) {
-                            sts.selectedIndex = idx;
-                            break;
-                        }
+                }
+                lfn ('txnid '+tid+', onreadystatechange(): READY STATE 4, RESPONSE TEXT FOUND');
+                tgt.value                   = xhr.responseText; 
+            }
+            xhr.onerror = function() {
+                for (var idx in sts.options) {
+                    if (sts.options[idx].value==xhr.status) {
+                        sts.selectedIndex = idx;
+                        break;
                     }
-                    lfn ('txnid '+tid+', onloadend(): LOADING COMPLETED');
-                    lfn ('txnid '+tid+', onloadend(): parsing codes and messages');
-                    var code        = '0';
-                    try {
-                        var object  = JSON.parse (tgt.value);
-                        var error   = object.response.error;
-                        this.log ('txnid '+tid+', onloadend(): object error='+error);
-                        var warning = object.response.warning;
-                        var notice  = object.response.notice;
+                }
+                tgt.value = '002 HTTP error '+xhr.status;
+                lfn ('txnid '+tid+', onerror(): HTTP ERROR');
+            }
+            xhr.ontimeout = function() {
+                for (var idx in sts.options) {
+                    if (sts.options[idx].value==599) {
+                        sts.selectedIndex = idx;
+                        break;
                     }
-                    catch (e) {
-                        var error   = tgt.value;
-                        var warning = '';
-                        var notice  = '';
+                }
+                tgt.value = '001 Request timed out';
+                lfn ('txnid '+tid+', ontimeout(): CONNECTION TIMED OUT');
+            }
+            xhr.onloadend = function() {
+            var s = xhr.status;
+                if (s==0) {
+                    s = 999;
+                }
+                for (var idx in sts.options) {
+                    if (sts.options[idx].value==s) {
+                        sts.selectedIndex = idx;
+                        break;
                     }
-                    lfn ('txnid '+tid+', onloadend(): system error='+error);
-                    // Extract codes from error message
-                    if (error!=null && error.length) {
-                        parts           = error.split (' ');
+                }
+                lfn ('txnid '+tid+', onloadend(): LOADING COMPLETED');
+                lfn ('txnid '+tid+', onloadend(): parsing codes and messages');
+            var code        = '0';
+                try {
+                var object  = JSON.parse (tgt.value);
+                var error   = object.response.error;
+                    this.log ('txnid '+tid+', onloadend(): object error='+error);
+                var warning = object.response.warning;
+                var notice  = object.response.notice;
+                }
+                catch (e) {
+                var error   = tgt.value;
+                var warning = '';
+                var notice  = '';
+                }
+                lfn ('txnid '+tid+', onloadend(): system error='+error);
+                // Extract codes from error message
+                if (error!=null && error.length) {
+                    parts           = error.split (' ');
+                    if (parts[0].match(/^[0-9]+$/)) {
+                        code    = parts[0];
+                        parts.shift ();
                         if (parts[0].match(/^[0-9]+$/)) {
-                            code    = parts[0];
-                            parts.shift ();
-                            if (parts[0].match(/^[0-9]+$/)) {
-                                for (var idx in sts.options) {
-                                    if (sts.options[idx].value==parts[0]) {
-                                        sts.selectedIndex = idx;
-                                        break;
-                                    }
+                            for (var idx in sts.options) {
+                                if (sts.options[idx].value==parts[0]) {
+                                    sts.selectedIndex = idx;
+                                    break;
                                 }
-                                parts.shift ();
                             }
-                            error   = parts.join (' ');
+                            parts.shift ();
                         }
+                        error   = parts.join (' ');
                     }
-                    // Load codes and messages into form
-                    cod.value       = code;
-                    erm.value       = error;
-                    wnm.value       = warning;
-                    ntc.value       = notice;
-                    tgt.classList.add ('completed');
-                    lfn ('txnid '+tid+', onloadend(): READY');
-                    // Click the ready button
-                    rdy.click ();
                 }
+                // Load codes and messages into form
+                cod.value       = code;
+                erm.value       = error;
+                wnm.value       = warning;
+                ntc.value       = notice;
+                tgt.classList.add ('completed');
+                lfn ('txnid '+tid+', onloadend(): READY');
+                // Click the ready button
+                rdy.click ();
+            }
             // Event handlers done
             // Initialise request
-                sts.selectedIndex = 0;
-                xhr.open ('POST',url,true);
-                xhr.timeout = toc;
-                this.log ('JSON to post:');
-                this.log (frm.json.value);
-                this.log ('txnid '+tid+', Setting request header Content-Type: application/json');
-                xhr.setRequestHeader ("Content-Type","application/json");
-                this.log ('txnid '+tid+', Sending request...');
-                xhr.send (frm.json.value);
+            sts.selectedIndex = 0;
+            xhr.open ('POST',url,true);
+            xhr.timeout = toc;
+            this.log ('JSON to post:');
+            this.log (frm.json.value);
+            this.log ('txnid '+tid+', Setting request header Content-Type: application/json');
+            xhr.setRequestHeader ("Content-Type","application/json");
+            this.log ('txnid '+tid+', Sending request...');
+            xhr.send (frm.json.value);
         }
 
     }
